278b784e5d31b2667832e295f2a6e462216c2da4
LAB14_USER ON WS14 <L14Y10W14@citulab.net>
2023-05-09 11:42:59 +0800

    changed preparedstatement to statement

--- src/main/java/com/csit228g4/huni/DBHelper.java
+++ src/main/java/com/csit228g4/huni/DBHelper.java
@@ -8,7 +8,7 @@ import java.sql.DriverManager;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.PreparedStatement;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -20,14 +20,14 @@ import java.util.logging.Logger;
 public class DBHelper {
     private final String url, username, password;
     private Connection conn;
-    private PreparedStatement stmt;
+    private Statement stmt;
     private ResultSet rs;
     
     public DBHelper() {
         // Connect to remote MySQL server
-        url = "jdbc:mysql://170.187.197.155:3306/dbHuni";
-        username = "huni";
-        password = "Huni_2023";
+        url = "jdbc:mysql://localhost:3306/dbHuni";
+        username = "root";
+        password = "";
     }
     
     public DBHelper(String url, String username, String password) {
@@ -46,6 +46,20 @@ public class DBHelper {
         }
     }
     
+    public boolean checkUser(String username) {
+        try {
+            String sql = "SELECT * FROM tblUser WHERE username='" + username + "'";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
+            
+            if (rs.next()) return true;
+        } catch (SQLException ex) {
+            Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        return false;
+    }
+    
     private String getCurrentDate() {
         // Get current date and convert to string
         return new java.text.SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(new java.util.Date());
@@ -55,21 +69,13 @@ public class DBHelper {
         // Add a new user to the database
         try {
             // Check if the user already exists
-            String sql = "SELECT * FROM tblUser WHERE username=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, username);
-            rs = stmt.executeQuery();
-            
-            // If the user already exists
-            if (rs.next()) return -1;
+            if (checkUser(username))
+                return -1;
             
             // Add the user data to the database
-            sql = "INSERT INTO tblUser (username, password, createdOn) VALUES (?, ?, ?)";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-            stmt.setString(3, getCurrentDate());
-            return stmt.executeUpdate();
+            String sql = "INSERT INTO tblUser (username, password, createdOn) VALUES ('" + username + "', '" + password + "'. '" + getCurrentDate() + "')";
+            stmt = conn.createStatement();
+            return stmt.executeUpdate(sql);
         } catch (SQLException ex) {
             Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -81,11 +87,9 @@ public class DBHelper {
         // Check user credentials
         try {
             // Check if user exists
-            String sql = "SELECT * FROM tblUser WHERE username=? AND password=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, username);
-            stmt.setString(2, password);
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblUser WHERE username='" + username + "' AND password='" + password + "'";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Return a default user instance if user not in database
             if (!rs.next()) return new User();
@@ -103,22 +107,17 @@ public class DBHelper {
         // Add a new playlist to the database
         try {
             // Check if the playlist already exists
-            String sql = "SELECT * FROM tblPlaylist WHERE name=? AND createdBy=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, name);
-            stmt.setString(2, String.valueOf(Session.activeUser.getId()));
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblPlaylist WHERE name='" + name + "' AND createdBy=" + Session.activeUser.getId();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // If the playlist already exists
             if (rs.next()) return -1;
             
             // Add playlist data into the database
-            sql = "INSERT INTO tblPlaylist (name, createdBy, createdOn) VALUES (?, ?, ?)";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, name);
-            stmt.setString(2, String.valueOf(Session.activeUser.getId()));
-            stmt.setString(3, getCurrentDate());
-            return stmt.executeUpdate();           
+            sql = "INSERT INTO tblPlaylist (name, createdBy, createdOn) VALUES ('" + name + "', " + Session.activeUser.getId() + ", '" + getCurrentDate() + "')";
+            stmt = conn.createStatement();
+            return stmt.executeUpdate(sql);
         } catch (SQLException ex) {
             Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);            
         }
@@ -129,10 +128,9 @@ public class DBHelper {
     public Playlist getPlaylist(int playlistId) {
         // Fetch playlist from database using id
         try {
-            String sql = "SELECT * FROM tblPlaylist WHERE id=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, String.valueOf(playlistId));
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblPlaylist WHERE id=" + playlistId;
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Create and return default instance if playlist does not exist
             if (!rs.next()) return new Playlist();
@@ -149,11 +147,9 @@ public class DBHelper {
     public Playlist getPlaylist(String name) {
         // Fetch playlist from database using name
         try {
-            String sql = "SELECT * FROM tblPlaylist WHERE name=? AND createdBy=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, name);
-            stmt.setString(2, String.valueOf(Session.activeUser.getId()));
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblPlaylist WHERE name='" + name + "' AND createdBy=" + Session.activeUser.getId();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Create and return default instance if playlist does not exist
             if (!rs.next())
@@ -172,10 +168,9 @@ public class DBHelper {
         // Fetch all playlists made by currently logged in user
         ArrayList<Playlist> playlists = new ArrayList<>();
         try {
-            String sql = "SELECT * FROM tblPlaylist WHERE createdBy=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, String.valueOf(Session.activeUser.getId()));
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblPlaylist WHERE createdBy=" + Session.activeUser.getId();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Add new playlist instance to array list
             while (rs.next())
@@ -192,11 +187,9 @@ public class DBHelper {
         int res = 0;
         try {
             // Update playlist
-            String sql = "UPDATE tblPlaylist SET name=? WHERE id=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, pl.getName());
-            stmt.setString(2, String.valueOf(pl.getId()));
-            res = stmt.executeUpdate();
+            String sql = "UPDATE tblPlaylist SET name='" + pl.getName() + "' WHERE id=" + pl.getId();
+            stmt = conn.createStatement();
+            res = stmt.executeUpdate(sql);
         } catch (SQLException ex) {
             Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);              
         }
@@ -214,16 +207,14 @@ public class DBHelper {
             if (pl.getId() == -1) return -1;
             
             // Delete matching playlist from the database
-            String sql = "DELETE FROM tblPlaylist WHERE name=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, pl.getName());
-            res = stmt.executeUpdate();
+            String sql = "DELETE FROM tblPlaylist WHERE name='" + playlistName + "'";
+            stmt = conn.createStatement();
+            res = stmt.executeUpdate(sql);
             
             // Delete all songs from the playlist
-            sql = "DELETE FROM tblPlaylistSong WHERE playlistId=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, String.valueOf(pl.getId()));
-            res = stmt.executeUpdate();
+            sql = "DELETE FROM tblPlaylistSong WHERE playlistId=" + pl.getId();
+            stmt = conn.createStatement();
+            stmt.executeUpdate(sql);
         } catch (SQLException ex) {
             Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -236,11 +227,9 @@ public class DBHelper {
         Playlist pl = null;
         try {
             // Retrieve specified playlist from database
-            String sql = "SELECT * FROM tblPlaylist WHERE name=? AND createdBy=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, playlistName);
-            stmt.setString(2, String.valueOf(Session.activeUser.getId()));
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblPlaylist WHERE name='" + playlistName + "' AND createdBy=" + Session.activeUser.getId();
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Create a playlist instance
             if (rs.next()) 
@@ -250,10 +239,9 @@ public class DBHelper {
             if (pl == null) return songs;
             
             // Find all songs that belongs to playlist
-            sql = "SELECT * FROM tblPlaylistSong WHERE playlistId=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, String.valueOf(pl.getId()));
-            ResultSet plSongs =  stmt.executeQuery();
+            sql = "SELECT * FROM tblPlaylistSong WHERE playlistId=" + pl.getId();
+            stmt = conn.createStatement();
+            ResultSet plSongs =  stmt.executeQuery(sql);
             
             // Create and add song instance to array list
             while (plSongs.next())
@@ -267,10 +255,9 @@ public class DBHelper {
     public Song getSong(int songId) {
         // Fetch a song from the database by id
         try {
-            String sql = "SELECT * FROM tblSong WHERE id=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, String.valueOf(songId));
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblSong WHERE id=" + songId;
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Create and return a song instance if a song is found
             if (rs.next())
@@ -284,10 +271,9 @@ public class DBHelper {
     public Song getSong(String title) {
         // Fetch a song from the database by title
         try {
-            String sql = "SELECT * FROM tblSong WHERE title=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, title);
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblSong WHERE title='" + title + "'";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Create and return a song instance if a song is found
             if (rs.next())
@@ -301,11 +287,9 @@ public class DBHelper {
     public Song getSong(String title, String author) {
         // Fetch a song from the database by title and author
         try {
-            String sql = "SELECT * FROM tblSong WHERE title=? AND author=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, title);
-            stmt.setString(2, author);
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblSong WHERE title='" + title + "' AND author='" + author + "'";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Create and return a song instance if a song is found
             if (rs.next())
@@ -321,8 +305,8 @@ public class DBHelper {
         ArrayList<Song> songs = new ArrayList<>();
         try {
             String sql = "SELECT * FROM tblSong";
-            stmt = conn.prepareStatement(sql);
-            rs =  stmt.executeQuery();
+            stmt = conn.createStatement();
+            rs =  stmt.executeQuery(sql);
            
             // Create a song instance and add to the list
             while (rs.next())
@@ -338,25 +322,17 @@ public class DBHelper {
         // Adds a new song to the database
         try {
             // Check if the song already exists
-            String sql = "SELECT * FROM tblSong WHERE title=? AND author=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, title);
-            stmt.setString(2, author);
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblSong WHERE title='" + title + "' AND author='" + author + "'";
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Exit if a song already exists
             if (rs.next()) return -1;
             
             // Add song to the database
-            sql = "INSERT INTO tblSong (source, title, author, url, thumbnail) VALUES (?, ?, ?, ?, ?)";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, "test");
-            stmt.setString(2, title);
-            stmt.setString(3, author);
-            stmt.setString(4, "test");
-            stmt.setString(5, "test");
-
-            return stmt.executeUpdate();
+            sql = "INSERT INTO tblSong (source, title, author, url, thumbnail) VALUES ('test', '" + title + "', '" + author + "', 'test', 'test')";
+            stmt = conn.createStatement();
+            return stmt.executeUpdate(sql);
         } catch (SQLException ex) {
             Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -367,23 +343,17 @@ public class DBHelper {
         // Add a song to a playlist
         try {
             // Check if the song is already in the playlist
-            String sql = "SELECT * FROM tblPlaylistSong WHERE playlistId=? AND songId=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, String.valueOf(playlistId));
-            stmt.setString(2, String.valueOf(songId));
-            rs = stmt.executeQuery();
+            String sql = "SELECT * FROM tblPlaylistSong WHERE playlistId=" + playlistId + " AND songId=" + songId;
+            stmt = conn.createStatement();
+            rs = stmt.executeQuery(sql);
             
             // Exit if song is already in the playlist
             if (rs.next()) return -1;
             
             // Add song to playlist
-            sql = "INSERT INTO tblPlaylistSong (playlistId, songId, addedOn) VALUES (?, ?, ?)";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, String.valueOf(playlistId));
-            stmt.setString(2, String.valueOf(songId));
-            stmt.setString(3, getCurrentDate());
-            
-            return stmt.executeUpdate();
+            sql = "INSERT INTO tblPlaylistSong (playlistId, songId, addedOn) VALUES (" + playlistId + ", " + songId + ", '" + getCurrentDate() + "')";
+            stmt = conn.createStatement();
+            return stmt.executeUpdate(sql);
         } catch (SQLException ex) {
             Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);
         }
@@ -394,12 +364,9 @@ public class DBHelper {
         // Remove song from playlist
         int res = 0;
         try {
-            String sql = "DELETE FROM tblPlaylistSong WHERE playlistId=? AND songId=?";
-            stmt = conn.prepareStatement(sql);
-            stmt.setString(1, String.valueOf(playlistId));
-            stmt.setString(2, String.valueOf(songId));
-            
-            res = stmt.executeUpdate();
+            String sql = "DELETE FROM tblPlaylistSong WHERE playlistId=" + playlistId + " AND songId=" + songId;
+            stmt = conn.createStatement();
+            res = stmt.executeUpdate(sql);
         } catch (SQLException ex) {
             Logger.getLogger(DBHelper.class.getName()).log(Level.SEVERE, null, ex);            
         }
